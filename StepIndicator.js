0f30f8ff5bd060b384f867bb98e084f3d8d96936
Saalihou Ndiaye <saalihou@gmail.com>
2018-10-05 17:30:46 +0000

    Add labelAlign prop (I hate this library)

--- StepIndicator.js
+++ StepIndicator.js
@@ -183,10 +183,9 @@ export default class StepIndicator extends PureComponent {
       });
 
       return(
-        <View style={[
-          styles.stepLabelsContainer,
+        <View style={[styles.stepLabelsContainer,
           direction === 'vertical' ? {flexDirection: 'column', paddingHorizontal:4} : {flexDirection: 'row', paddingVertical:4},
-          { alignItems: {'left': 'flex-start', 'right': 'flex-end', 'center': 'center'}[this.state.customStyles.labelTextAlign]}
+          {alignItems: this.customStyles.labelAlign}
         ]}>
           {labelViews}
         </View>
